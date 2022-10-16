2addc5ebdf4aeb1d5e31506d2d9fa7b39c8eb20e
Safwa9amar <hassanih97@gmail.com>
2022-10-16 14:40:28 +0100

    socket

--- static/js/socket.js
+++ static/js/socket.js
@@ -1,5 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const socket = io(`http://${document.domain}:${location.port}/test`);
+  const socket = io(`http://${document.domain}:${location.port}/test`, {
+    transports: ["websocket"],
+    upgrade: false,
+  });
 
   try {
     let notification_indicator = document.getElementById(
@@ -11,26 +14,25 @@ document.addEventListener("DOMContentLoaded", () => {
   socket.emit("message", {
     test: "server side connected from socket file",
   });
-  // setTimeout(() => {
-  //   socket.on("message", function (data) {
-  //     console.log("msg from server");
-  //     try {
-  //       notification_indicator.textContent = JSON.parse(data).length;
-  //       let html = `
-  //     <a href="#" class="flex gap-4 items-stratch">
-  //       <div class="avatar">
-  //         <div class="w-10 rounded-full">
-  //           <img src="https://placeimg.com/192/192/people" />
-  //         </div>
-  //       </div>
-  //       <div class="flex flex-col gap-2">
-  //         <h1>john doe a passé une commande</h1>
-  //         <p>29 July 2020 - 02:26 PM</p>
-  //       </div>
-  //     </a>
-  //     `;
-  //       notification_area.innerHTML = html;
-  //     } catch (error) {}
-  //   });
-  // }, 3000);
+
+  socket.on("message", function (data) {
+    console.log("msg from server");
+    try {
+      notification_indicator.textContent = JSON.parse(data).length;
+      let html = `
+      <a href="#" class="flex gap-4 items-stratch">
+        <div class="avatar">
+          <div class="w-10 rounded-full">
+            <img src="https://placeimg.com/192/192/people" />
+          </div>
+        </div>
+        <div class="flex flex-col gap-2">
+          <h1>john doe a passé une commande</h1>
+          <p>29 July 2020 - 02:26 PM</p>
+        </div>
+      </a>
+      `;
+      notification_area.innerHTML = html;
+    } catch (error) {}
+  });
 });
