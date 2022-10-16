aa338419b87cc53fb75bbabeb86e97cf3b7c508e
Safwa9amar <hassanih97@gmail.com>
2022-10-16 14:37:58 +0100

    socket

--- static/js/socket.js
+++ static/js/socket.js
@@ -11,27 +11,26 @@ document.addEventListener("DOMContentLoaded", () => {
   socket.emit("message", {
     test: "server side connected from socket file",
   });
-  setTimeout(() => {
-    socket.off("message");
-  }, 3000);
-  socket.on("message", function (data) {
-    console.log("msg from server");
-    try {
-      notification_indicator.textContent = JSON.parse(data).length;
-      let html = `
-      <a href="#" class="flex gap-4 items-stratch">
-        <div class="avatar">
-          <div class="w-10 rounded-full">
-            <img src="https://placeimg.com/192/192/people" />
-          </div>
-        </div>
-        <div class="flex flex-col gap-2">
-          <h1>john doe a passé une commande</h1>
-          <p>29 July 2020 - 02:26 PM</p>
-        </div>
-      </a>
-      `;
-      notification_area.innerHTML = html;
-    } catch (error) {}
-  });
+  // setTimeout(() => {
+  //   socket.on("message", function (data) {
+  //     console.log("msg from server");
+  //     try {
+  //       notification_indicator.textContent = JSON.parse(data).length;
+  //       let html = `
+  //     <a href="#" class="flex gap-4 items-stratch">
+  //       <div class="avatar">
+  //         <div class="w-10 rounded-full">
+  //           <img src="https://placeimg.com/192/192/people" />
+  //         </div>
+  //       </div>
+  //       <div class="flex flex-col gap-2">
+  //         <h1>john doe a passé une commande</h1>
+  //         <p>29 July 2020 - 02:26 PM</p>
+  //       </div>
+  //     </a>
+  //     `;
+  //       notification_area.innerHTML = html;
+  //     } catch (error) {}
+  //   });
+  // }, 3000);
 });
